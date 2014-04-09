921e6e16ade825397cb91292f0d852e2b3d4e029
David Wise <dhmwise@hotmail.com>
2014-04-09 17:08:15 +0200

    First attempt, solved it

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
@@ -1,11 +1,23 @@
 def bank_score
   # TODO: Use Random to get a new random score
+
+  bank_score = random(16; 21)
+
+  bank_score
+
 end
 
 def pick_card
   # TODO: Use Random to get a new random card
+
+  pick_card = random(1; 11)
+
+  pick_card
 end
 
 def game_outcome(bank, score)
   # TODO: Take the bank and the score and output an array containing the bank and then the score
+
+  game_outcome = (bank + score)
+
 end
