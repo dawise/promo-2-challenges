060d0789f3c6b971d86dc74ee2c0bea427bb63ea
David Wise <dhmwise@hotmail.com>
2014-04-21 11:41:41 +0200

    premiere partie

--- 02-OOP/08-Cookbook/lib/cookbook.rb
+++ 02-OOP/08-Cookbook/lib/cookbook.rb
@@ -1,9 +1,54 @@
+require "csv"
+
 class Cookbook
+
+  attr_reader :contents
+
   def initialize(file)
     # TODO: Retrieve the data from your CSV file and store it in an instance variable
+    @file = file
+    load_csv
   end
+
+  def load_csv
+    @contents = []
+    CSV.foreach(@file) do |row| @contents << row.first
+    end
+  end
+
+  # def retrieve(contents)
+  #   "here are your recipes : "
+  #   @contents.each_with_index do |item, index|
+  #     "#{index+1}. #{item}"
+  #   end
+  # end
+
+  def delete_one_recipe(index)
+    @contents.delete_at(index-1)
+    CSV.open(@file, 'w') do |csv|
+      @contents.each do |name|
+        csv << [name]
+      end
+    end
+  end
+
+  def add_recipe(name_recipe)
+    @contents << name_recipe
+    CSV.open(@file, "a") do |csv|
+    csv << [name_recipe]
+   end
+    end
 
   # TODO: Implement the methods to retrieve all recipes, create, or destroy recipes
   # TODO: Implement a save method that will write the data into the CSV
   # And don't forget to use this save method when you have to modify something in your recipes array.
 end
+
+#my_cookbook = CookBook.new ("lib/cookbook/recipes.csv")
+
+
+
+
+
+
+
--- 02-OOP/08-Cookbook/lib/ui.rb
+++ 02-OOP/08-Cookbook/lib/ui.rb
@@ -1,3 +1,4 @@
+
 class UI
   TASKS = {
     list: "- List all recipes [list]",
@@ -14,16 +15,25 @@ class UI
   def list
     # TODO: call the appropriate controller method with the proper argument(s)
     # TODO: format and display the retrieved data in a numbered list
+    @controller.list_recipes.each_with_index do |item, index|
+      puts "#{index +1}. #{item}"
+    end
   end
 
   def add
     # TODO: ask the user a recipe name
     # TODO: call the appropriate controller method with the proper argument(s)
+    puts "Which recipe do you want to add ?"
+    name_recipe = gets.chomp
+    @controller.add(name_recipe)
   end
 
   def del
     # TODO: ask the user a recipe index
     # TODO: call the appropriate controller method with the proper argument(s)
+    puts "Which recipe do you want to delete ?"
+    index = gets.chomp.to_i
+    @controller.delete(index)
   end
 
   def exit
@@ -35,6 +45,7 @@ class UI
     # TODO: Get the user input and return it
     # [OPTIONAL] You can think of the case where the user
     # enters a wrong choice.
+    gets.chomp
   end
 
   def display
@@ -70,4 +81,5 @@ class UI
   def dispatch(task)
     self.send(task.to_sym)
   end
+
 end
