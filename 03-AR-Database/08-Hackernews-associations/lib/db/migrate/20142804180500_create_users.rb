73d7fbc17ce04ff27a633221f53e95a7fef33a96
David Wise <dhmwise@hotmail.com>
2014-04-28 18:47:52 +0200

    ex8

---
+++ 03-AR-Database/08-Hackernews-associations/lib/db/migrate/20142804180500_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+
+  def change
+    create_table :users do |table|
+      table.string :name
+      table.string :email
+    end
+  end
+
+
+end
--- 03-AR-Database/08-Hackernews-associations/lib/interface.rb
+++ 03-AR-Database/08-Hackernews-associations/lib/interface.rb
@@ -14,7 +14,18 @@ while true
 
   until logged_in
     puts "Please login with your <id>"
+    id = gets.chomp.toi_i
+
+    begin
+    user = User.find(id)
+    logged_in = true
+
+    rescue #RecordNotFound => e
+      puts "Error !"
+      logged_in = false
+    end
     #TODO: instantiate a user with his <id>
+
   end
 
   puts "Hey #{user.name}, what do you want to do today? Enter <task_id>"
@@ -22,22 +33,27 @@ while true
   puts "2. Read your posts"
   puts "3. Delete all posts"
   puts "4. Exit"
-  
+
 	choice =  gets.chomp.to_i
-	
+
 	case choice
   when 1
     name = ask_and_get("name")
     source_url = ask_and_get("source url")
     rating = ask_and_get("rating")
-    post = { name: name, source_url: source_url, date: Time.now, rating: rating }
+    post = { name: name, source_url: source_url, date: Time.now,
+     rating: rating, :user_id user.id}
     #TODO: use ActiveRecord to add a new post for the user logged in!
+    Post.create (post)
   when 2
     #TODO: use ActiveRecord to get all posts of the current user
+    user.posts.each do |post|
+      puts post
   when 3
-    #TODO: use ActiveRecord to delete all posts of current user 
-  when 4 
+    #TODO: use ActiveRecord to delete all posts of current user
+    user.posts.delete_all
+  when 4
     break
-	end 
-	
+	end
+
 end
