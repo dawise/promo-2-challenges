ac1326c4c1a3325b91c2b878716bf5a353ed2327
David Wise <dhmwise@hotmail.com>
2014-04-28 17:22:11 +0200

    hackernews

--- 03-AR-Database/07-Hackernews-AR-basics/lib/interface.rb
+++ 03-AR-Database/07-Hackernews-AR-basics/lib/interface.rb
@@ -13,22 +13,27 @@ while true
   puts "2. Read your posts"
   puts "3. Delete all posts"
   puts "4. Exit"
-  
+
 	choice =  gets.chomp.to_i
-	
+
 	case choice
   when 1
     name = ask_and_get("name")
     source_url = ask_and_get("source url")
     rating = ask_and_get("rating")
-    post = { name: name, source_url: source_url, date: Time.now, rating: rating }
     #TODO: use ActiveRecord to add a new entry to the DB
+    post = Post.create(name: name, source_url: source_url, date: Time.now, rating: rating)
   when 2
     #TODO: use ActiveRecord to get all entries from the DB
+    posts = Post.all
+    puts "Post name is #{post.name},
+    from #{post.source_url},
+    published on #{post.date}, rated #{post.rating} "
   when 3
     #TODO: use ActiveRecord to delete all entries from the DB
-  when 4 
+    Post.destroy_all
+  when 4
     break
-	end 
-	
+	end
+
 end
